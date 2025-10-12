9d418519a20289dfa0bf0279269422cb8226e4d2
faisalljavid <faisalljavid@gmail.com>
2025-10-13 02:39:54 +0530

    Add authentication integration with Clerk

---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+    return (
+        <div>
+            Only loggedin users should see this
+        </div>
+    );
+};
+
+export default page;
--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,16 +1,28 @@
-import { Button } from "@/components/ui/button"
-import { UserCircleIcon } from "lucide-react"
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { UserCircleIcon } from "lucide-react";
+import { UserButton, SignInButton, SignUpButton, SignedOut } from "@clerk/nextjs";
 
 export const AuthButton = () => {
-    // TODO: Add different auth states
     return (
-        <Button
-            variant="outline"
-            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20
-            rounded-full shadow-none"
-        >
-            <UserCircleIcon />
-            Sign in
-        </Button>
+        <>
+            <SignInButton>
+                <UserButton />
+                {/* Add menu items for studio and user profile */}
+            </SignInButton>
+            <SignedOut>
+                <SignInButton mode="modal">
+                    <Button
+                        variant="outline"
+                        className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20
+                        rounded-full shadow-none"
+                    >
+                        <UserCircleIcon />
+                        Sign in
+                    </Button>
+                </SignInButton>
+            </SignedOut>
+        </>
     )
 } 
